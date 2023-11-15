f7601b4c0710008f98c08f4219781089c4a8ecef
Aleksi <aleksi.saxlund@gmail.com>
2023-11-15 14:36:19 +0200

    tasks

---
+++ tasks.py
@@ -0,0 +1,13 @@
+from invoke import task
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
