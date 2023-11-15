e08cc54a0c41db7ea642bec4f6562c88f3c07391
Heidi Putkuri <heidi.putkuri@helsinki.fi>
2023-11-15 15:40:16 +0200

    Routes.py

--- src/routes.py
+++ src/routes.py
@@ -3,5 +3,10 @@ from flask import Flask, render_template, redirect
 
 @app.route("/")
 def index():
+    """Render index"""
     return render_template("index.html")
 
+@app.route("/new_reference")
+def new_reference():
+    """Render form."""
+    return render_template("form.html")
