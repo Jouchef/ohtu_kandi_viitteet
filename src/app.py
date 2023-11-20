46e7347e3fddd95d7491f481161e6543ccc45167
Maija Rislakki <maija.rislakki@helsinki.fi>
2023-11-20 16:14:44 +0200

    uusi app

--- src/app.py
+++ src/app.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from os import getenv
+from database import db
+from os import environ
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 
-# fmt: off
-import routes
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URL")
+
+db.init_app(app)
+
+import routes  # nopep8
