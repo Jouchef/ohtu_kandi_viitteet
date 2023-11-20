2be8daf488546915a49692bd0e718e3fe0e0dfe1
Maija Rislakki <maija.rislakki@helsinki.fi>
2023-11-20 15:23:37 +0200

    korjattu viittaus

--- src/save_citate_to_db.py
+++ src/save_citate_to_db.py
@@ -4,10 +4,10 @@ from database import db
 
 
 def citate_to_db(self, author, title, book_title, journal, year, volume, pages, publisher):
-    sql = text("INSERT INTO citate (author, title, book_title, journal, year, volume, pages, publisher) VALUES (:self, author, title, book_title, journal, year, volume, pages, publisher)")
+    sql = text("INSERT INTO References (author, title, booktitle, journal, year, volume, pages, publisher) VALUES (:self, author, title, book_title, journal, year, volume, pages, publisher)")
 
     try:
-        result = db.session.execute(sql, {"author": author, "title": title, "book_title": book_title,
+        result = db.session.execute(sql, {"author": author, "title": title, "booktitle": book_title,
                                     "journal": journal, "year": year, "volume": volume, "pages": pages, "publisher": publisher})
         db.session.commit()
 
