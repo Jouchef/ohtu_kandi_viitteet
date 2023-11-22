666ad0b956c2eebf2be263755b2952d89073d8e7
Elisa Mero <elisa.mero@helsinki.fi>
2023-11-22 15:10:21 +0200

    frontend

--- src/routes.py
+++ src/routes.py
@@ -37,3 +37,23 @@ def edit_reference():
     # pitää hakea halutun viitteen tiedot tietokannasta! -> tallenna tiedot listaan "info=[]"
     # info=info
     return render_template("edit.html")
+
+
+@app.route("/make_changes", methods=["GET", "POST"])
+def make_changes():
+    author = request.form["name"]
+    title = request.form["title"]
+    book_title = request.form["book_title"]
+    journal = request.form["journal"]
+    year = request.form["year"]
+    volume = request.form["volme"]
+    pages = request.form["pages"]
+    publisher = request.form["publisher"]
+    sql_queries.make_changes(
+        author, title, book_title, journal, year, volume, pages, publisher)
+    return render_template("index.html")
+
+
+@app.route("/delete_reference", methods=["GET", "POST"])
+def delete_reference():
+    pass
