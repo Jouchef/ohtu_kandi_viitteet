2ff52af6fb5a82ccec7de95eb586cf103a9a3ee5
Elisa Mero <elisa.mero@helsinki.fi>
2023-11-22 14:28:40 +0200

    testi

--- src/main.py
+++ src/main.py
@@ -2,13 +2,15 @@
 from citationArticle import CitationArticle
 from generateCitate import GenerateCitate
 
+
 def main():
-    citation = CitationArticle("article", "CitekeyArticle", "P. J. Cohen", 
-        "The independence of the continuum hypothesis", 
-        "Proceedings of the National Academy of Sciences", 
-        1963, "50", "6", "1143--1148")
-    #print(citation)
+    citation = CitationArticle("article", "CitekeyArticle", "P. J. Cohen",
+                               "The independence of the continuum hypothesis",
+                               "Proceedings of the National Academy of Sciences",
+                               1963, "50", "6", "1143--1148")
+    # print(citation)
     print(citation.citation_to_bibtex_entry())
+
 
 if __name__ == "__main__":
     main()
