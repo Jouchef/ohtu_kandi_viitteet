5bc0f44dd17083c4cf2ab8b9f96062bf1d48ceb1
Maija Rislakki <maija.rislakki@helsinki.fi>
2023-11-27 13:44:51 +0200

    korjaus

--- src/database.py
+++ src/database.py
@@ -1,10 +1,10 @@
 from os import getenv
 from extensions import db, app
 
-<<<<<<< HEAD
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://maija:risla47@localhost/ohtu'
+<< << << < HEAD
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
-=======
+== == == =
 db.init_app(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
->>>>>>> 25ec1af36744ac1f33dd54adaad39ca40e35d6be
+>>>>>> > 25ec1af36744ac1f33dd54adaad39ca40e35d6be
