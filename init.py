77c61e6084cd33f78eacbd6091023ba944b931b0
Heidi Putkuri <59087733+heksaani@users.noreply.github.com>
2023-11-28 00:23:23 +0200

    Update init.py
    Typo

--- init.py
+++ init.py
@@ -24,7 +24,6 @@ def initialize_database():
         conn = psycopg2.connect(
             dbname=DB_NAME,
             user=DB_USER,
-            password=DB_PASSWORD,
             host=DB_HOST,
             port=DB_PORT
         )
