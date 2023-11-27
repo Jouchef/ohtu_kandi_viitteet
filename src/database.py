cffaa8a1198a7da97529a8c1a76ea0b66a75f20d
Maija Rislakki <maija.rislakki@helsinki.fi>
2023-11-27 16:19:56 +0200

    taulukkomuoto yritys

--- src/database.py
+++ src/database.py
@@ -2,5 +2,5 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from extensions import db, app
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://maija:risla47@localhost/ohtu'
 db = SQLAlchemy(app)
