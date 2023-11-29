bb5d187c0aa4ea5d2ab483e4a08fd2adba37ea7b
Aleksi <aleksi.saxlund@gmail.com>
2023-11-29 13:54:32 +0200

    query korjauksia

--- src/sql_queries.py
+++ src/sql_queries.py
@@ -28,7 +28,7 @@ def article_to_db(article: dict):
         "INSERT INTO References_Table (author, title, journal, year, volume, number, pages, month, doi, note, key) VALUES (:author, :title, :journal, :year, :volume, :number, :pages, :month, :doi, :note, :key)")
     try:
         cur = conn.cursor()
-        conn.execute(sql, author=author, title=title, journal=journal, year=year, volume=volume, number=number,
+        cur.execute(sql, author=author, title=title, journal=journal, year=year, volume=volume, number=number,
                      pages=pages, month=month, doi=doi, note=note, key=key)
         conn.commit()
         cur.close()
@@ -45,10 +45,11 @@ def all_references_from_db():
 
     try:
         cur = conn.cursor()
-        result = cur.execute(text(sql))
+        result = cur.execute(sql)
         references = result.fetchall() # this is a list of tuples
         cur.close()
         conn.close()
+
         return references
     except Exception as e:
         print (e, "error")
@@ -60,14 +61,15 @@ def search_by_name_from_db(search):
         "SELECT * FROM References_Table WHERE visible = 1 AND LOWER(author) LIKE LOWER(:author) ORDER BY author ASC")
 
     try:
-        result = db.session.execute(sql, {"author": f"%{search}%"}).fetchall()
-
-        db.session.commit()
+        cur = conn.cursor()
+        result = cur.execute(sql, {"author": f"%{search}%"})
+        reference = result.fetchall() # this is a list of tuples
+        cur.close()
+        conn.close()
 
         return result
-
     except Exception as e:
-        db.session.rollback()
+        print(e, "error")
         return None
 
 
@@ -75,41 +77,45 @@ def change_visible_to_false():
     sql = text("UPDATE References_Table SET visible = 0 WHERE id = :id")
 
     try:
-        result = db.session.execute(sql, {"id": id})
-
-        db.session.commit()
+        cur = conn.cursor()
+        cur.execute(sql, {"id": id})
+        conn.commit()
+        cur.close()
+        conn.close()
 
         return
     except Exception as e:
-        db.session.rollback()
+        cur.rollback()
+        print(e, "error")
         return None
 
-
-def edit_queries():
-    pass
-    # sql = text(
-    #  "SELECT author, title, book_title, journal, year, volume, pages, publisher WHERE ")
-
-    # haetaan listaan kaikki sitaatit
-
-
 def ids_list():
-    ids_list = []
-    sql = text("SELECT id FROM references_table WHERE visible = TRUE")
-    data = db.session.execute(sql)
-    data = data.fetchall()
-    for id in data:
-        ids_list.append(id[0])
-    return ids_list
+    sql = text("SELECT id FROM References_Table WHERE visible = TRUE")
+    try:
+        cur = conn.cursor()
+        cur.execute(sql)
+        result = cur.fetchall()
+        cur.close()
+        conn.close()
+
+        ids_list = []
+        for id in result:
+            ids_list.append(id[0])
+
+        return ids_list
+    except Exception as e:
+        print(e, "error")
+        return None
 
 
 def citates_to_list():
     citates_dict = {}
     print(ids_list())
     for index, citate_id in enumerate(ids_list()):
+        cur = conn.cursor()
         sql = text(
             "SELECT author, title, year, type FROM References_Table WHERE id = :id")
-        result = db.session.execute(sql, {"id": citate_id}).fetchall()
+        result = cur.execute(sql, {"id": citate_id}).fetchall()
         for author, title, year, citate_type in result:
             citates_dict[citate_id] = {
                 'author': author,
@@ -117,6 +123,9 @@ def citates_to_list():
                 'year': year,
                 'type': citate_type
             }
+        conn.commit()
+        cur.close()
+        conn.close()
     print(citates_dict)
     return citates_dict
 
@@ -134,15 +143,19 @@ def edit_queries(author, title, booktitle, journal, year, volume, pages, publish
                " WHERE id = :id")
 
     try:
-        result = db.session.execute(sql, {"author": author, "title": title, "booktitle": booktitle,
+        cur = conn.cursor()
+        cur.execute(sql, {"author": author, "title": title, "booktitle": booktitle,
                                           "journal": journal, "year": year, "volume": volume,
                                           "pages": pages, "publisher": publisher, "id": id})
 
-        db.session.commit()
+        conn.commit()
+        cur.close()
+        conn.close()
 
         return
     except Exception as e:
-        db.session.rollback()
+        cur.rollback()
+        print(e, "error")
         return None
 
 
