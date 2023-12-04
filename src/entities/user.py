20a21179c8a2bdffd9ca398e4122b3fc3959c2c5
alannesanni <sanni.alanne@helsinki.fi>
2023-12-04 17:05:32 +0200

    tyyppivihjeet

--- src/entities/user.py
+++ src/entities/user.py
@@ -1,6 +1,6 @@
 """User class definition."""
 class User:
-    def __init__(self, username, email, password):
+    def __init__(self, username: str, email: str, password: str):
         self.username = username
         self.email = email
         self.password = password
